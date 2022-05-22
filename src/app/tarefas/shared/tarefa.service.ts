43e7fd717cbd0b3c8a1b3436c8e5d453255a3508
Myckol Fontes <myckol.ti@gmail.com>
2022-05-22 17:51:02 -0300

    Criando metodo remover

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -34,11 +34,11 @@ export class TarefaService {
     localStorage['tarefas'] = JSON.stringify(tarefas);
   }
 
-  // remover(id: number): void {
-  //   let tarefas: Tarefa[] = this.listarTodos();
-  //   tarefas = tarefas.filter(tarefa => tarefa.id !== id);
-  //   localStorage['tarefas'] = JSON.stringify(tarefas);
-  // }
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
 
   // alterarStatus(id: number): void {
   //   const tarefas: Tarefa[] = this.listarTodos();
